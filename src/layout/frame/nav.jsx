48e175291feb2a616fab2b91e4c0f3649093b5e3
jialu.zjl <15879398507@163.com>
2020-11-01 16:49:22 +0800

    优化 babel 配置文件

--- src/layout/frame/nav.jsx
+++ src/layout/frame/nav.jsx
@@ -5,31 +5,31 @@ import routeUrl from '../../router'
 
 const { SubMenu } = Menu
 
-const menuMap = routes => {
-  return (
-    routes.map(item => {
-      if (item.children) {
-        return (
-          <SubMenu key={item.key} title={item.title}>
-            {menuMap(item.children)}
-          </SubMenu>
-        )
-      } else {
-        return (
-          <Menu.Item key={item.key}>
-            <Link to={item.url}>{item.title}</Link>
-          </Menu.Item>
-        )
-      }
-    })
-  )
-}
-
 class Nav extends Component {
+  menuMap = routes => {
+    return (
+      routes.map(item => {
+        if (item.children) {
+          return (
+            <SubMenu key={item.key} title={item.title}>
+              {this.menuMap(item.children)}
+            </SubMenu>
+          )
+        } else {
+          return (
+            <Menu.Item key={item.key}>
+              <Link to={item.url}>{item.title}</Link>
+            </Menu.Item>
+          )
+        }
+      })
+    )
+  }
+
   render() {
     return (
       <Menu mode="horizontal">
-        {menuMap(routeUrl)}
+        {this.menuMap(routeUrl)}
       </Menu>
     )
   }
